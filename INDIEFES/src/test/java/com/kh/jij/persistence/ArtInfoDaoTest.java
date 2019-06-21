5f650bcb683142815a74f206235cd29b8cebdd1d
dlswl123 <dlswl123@gmail.com>
2019-06-21 09:11:47 +0900

    test commit

---
+++ INDIEFES/src/test/java/com/kh/jij/persistence/ArtInfoDaoTest.java
@@ -0,0 +1,34 @@
+package com.kh.jij.persistence;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ArtInfoDaoTest {
+
+	@Test
+	public void testInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTeamInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTeamInput() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testArt_read() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMusic_read() {
+		fail("Not yet implemented");
+	}
+
+}
