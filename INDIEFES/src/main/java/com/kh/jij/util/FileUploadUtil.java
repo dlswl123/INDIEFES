ab7d98d358ff57844731876bae2fe1cd725e3246
Bob <Bob@email.com>
2019-06-21 09:38:24 +0900

    ji

--- INDIEFES/src/main/java/com/kh/jij/util/FileUploadUtil.java
+++ INDIEFES/src/main/java/com/kh/jij/util/FileUploadUtil.java
@@ -15,14 +15,16 @@ public class FileUploadUtil {
 		int teamNumber = artVo.getTeam_number();
 		int artNumber = artVo.getArt_number();
 		
-		String filePath = uploadPath+File.separator +teamNumber+File.separator+artNumber;
+		String filePath = uploadPath+File.separator +teamNumber+File.separator+artNumber+ File.separator + originalName;
+		String path = uploadPath+File.separator +teamNumber+File.separator+artNumber;
 		String fileName = teamNumber+File.separator+artNumber+File.separator+originalName;
 		
-		File target = new File(filePath);
+		File target = new File(path);
+		File f = new File(filePath);
 		if (!target.exists()) {
 			target.mkdirs();
 		}
-		FileCopyUtils.copy(fileData, target);
+		FileCopyUtils.copy(fileData, f);
 		
 		return fileName;
 	}
