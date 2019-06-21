80ede249796abcef9404951ec22e3a38b17bb4ca
YoonFate <899yoon@naver.com>
2019-06-21 12:46:45 +0900

    user_id only required method add logout function add

--- INDIEFES/src/main/java/com/kh/ks/service/IUserInfoService.java
+++ INDIEFES/src/main/java/com/kh/ks/service/IUserInfoService.java
@@ -4,4 +4,6 @@ import com.kh.ks.domain.UserInfoVo;
 
 public interface IUserInfoService {
 	public UserInfoVo readWithPw(String user_id, String user_pw) throws Exception;
+	
+	public UserInfoVo readWith(String user_id) throws Exception;
 }
