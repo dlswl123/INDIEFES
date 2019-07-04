b1073fa1e15e39f5bc58ee970b4fe6ecb1c31f94
Bob <Bob@email.com>
2019-07-04 12:53:22 +0900

    ji

--- INDIEFES/src/main/java/com/kh/jij/controller/MusicController.java
+++ INDIEFES/src/main/java/com/kh/jij/controller/MusicController.java
@@ -61,7 +61,7 @@ public class MusicController {
 			musicService.musicInsert(musicInfoVo);
 			String originalName = file.getOriginalFilename();
 			try {
-				FileUploadUtil.uploadFile(uploadPath, originalName, musicInfoVo, file.getBytes());
+				FileUploadUtil.musicUploadFile(uploadPath, originalName, musicInfoVo, file.getBytes());
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
